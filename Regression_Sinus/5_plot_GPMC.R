4e09dbe3f4d9b97dfc38431c29b00200b15b225f
Francois Bachoc <francois.bachoc@gmail.com>
2023-12-05 18:24:07 +0100

    plot update

--- Regression_Sinus/5_plot_GPMC.R
+++ Regression_Sinus/5_plot_GPMC.R
@@ -5,12 +5,13 @@ load("data.Rdata")
 
 load(name)
 pdf(nameFig) 
+par(mar = c(6, 6, 6, 6))
 plot(Xt,f(Xt),type="l",col="black",ylim=c(0,1.5),
-     cex.axis=1.5,cex.lab=1.5,cex=1.5,
+     cex.axis=2.5,cex.lab=2.5,cex=2.5,lwd=3,
      xlab="x",ylab="f")
-points(Xt,mytGP,type="l",col="blue",cex=1.5)
+points(Xt,mytGP,type="l",col="blue",cex=2.5)
 points(Xt,qitGP,type="l",col="green",
-       cex=1.5)
+       cex=2.5,lwd=3)
 points(Xt,qstGP,type="l",col="green",
-       cex=1.5)
+       cex=2.5,lwd=3)
 dev.off()
