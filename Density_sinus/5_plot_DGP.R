4eb256360f699b16d45e2e6f56b531f957b23b06
Francois Bachoc <francois.bachoc@gmail.com>
2023-12-05 18:22:58 +0100

    plot update

--- Density_sinus/5_plot_DGP.R
+++ Density_sinus/5_plot_DGP.R
@@ -5,22 +5,25 @@ load("data.Rdata")
 
 load(name)
 pdf(nameFig1) 
+par(mar = c(6, 6, 6, 6))
 plot(Xt,f(Xt),type="l",col="black",ylim=c(0,1.5),
-     cex.axis=1.5,cex.lab=1.5,cex=1.5,
+     cex.axis=2.5,cex.lab=2.5,cex=2.5,lwd=3,
      xlab="x",ylab="f")
-points(Xt,mytDGP,type="l",col="blue",cex=1.5)
+points(Xt,mytDGP,type="l",col="blue",cex=1.5,lwd=3)
 points(Xt,qitDGP,type="l",col="green",
-       cex=1.5)
+       cex=1.5,lwd=3)
 points(Xt,qstDGP,type="l",col="green",
-       cex=1.5)
+       cex=1.5,lwd=3)
 dev.off()
 
 pdf(nameFig2) 
+par(mar = c(6, 6, 6, 6))
 plot(Xt,phi(Xt,m)%*%samplesAlphaDGP[indKeepDGP[sample(x=nbatch/2,size=1)],1:m],
-     type="l",ylim=c(0,1),cex.axis=1.5,cex.lab=1.5,
-     cex=1.5,xlab="x",ylab="Z1")
+     type="l",ylim=c(0,1),
+     cex.axis=2.5,cex.lab=2.5,cex=2.5,lwd=3,
+     xlab="x",ylab="Z1")
 points(Xt,phi(Xt,m)%*%samplesAlphaDGP[indKeepDGP[sample(x=nbatch/2,size=1)],1:m],
-       type="l",col="blue")
+       type="l",col="blue",lwd=3)
 points(Xt,phi(Xt,m)%*%samplesAlphaDGP[indKeepDGP[sample(x=nbatch/2,size=1)],1:m],
-       type="l",col="red")
+       type="l",col="red",lwd=3)
 dev.off()
